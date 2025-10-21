18c118bbdf6b3b8cc75404c72cc2c620826642f4
Your Name <your.email@example.com>
2025-10-21 09:57:02 +0700

    Fix import error - move PurchaseOrder and Supplier from inventory to suppliers

--- myproject/main_dashboard/views.py
+++ myproject/main_dashboard/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
+from django.db.models import Q, F
+from inventory.models import Product
+from suppliers.models import PurchaseOrder, Supplier
 
 @method_decorator(login_required, name='dispatch')
 class MainDashboardView(TemplateView):
@@ -12,10 +15,14 @@ class MainDashboardView(TemplateView):
         context['title'] = 'OVNC - Trang chủ'
         
         # Add dashboard stats
-        context['products_count'] = 0
-        context['purchase_orders_count'] = 0
-        context['suppliers_count'] = 0
-        context['low_stock_count'] = 0
+        context['products_count'] = Product.objects.count()
+        context['purchase_orders_count'] = PurchaseOrder.objects.count()
+        context['suppliers_count'] = Supplier.objects.count()
+        
+        # Sản phẩm sắp hết: current_quantity <= minimum_quantity
+        context['low_stock_count'] = Product.objects.filter(
+            current_quantity__lte=F('minimum_quantity')
+        ).count()
         
         return context
         
